01d8af28430c9fe2d3e0e780e25fae1aa9e80ec9
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-06-25 15:59:34 -0400

    Able to send weekly test email

--- app/mailers/parent_mailer.rb
+++ app/mailers/parent_mailer.rb
@@ -1,9 +1,12 @@
 class ParentMailer < ApplicationMailer
-  def weekly_email
-    parents = Parent.all
-    parents.each do |p|
-      p.find_grades(p.email)
-      mail(to: p.email, subject: "Your child's weekly progress!")
-    end
+  def weekly_email(email)
+    @parent = Parent.find_by_email(email)
+    @student = Student.find_by_id(@parent.student_id)
+    @grades = @student.grades
+    # parents = Parent.all
+    # parents.each do |p|
+    #   p.find_grades(p.email)
+    mail(to: "#{ENV["GMAIL_USERNAME"]}@gmail.com", subject: "Your child's weekly progress!")
+  # end
   end
 end
--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,4 +1,5 @@
 class Grade < ActiveRecord::Base
   belongs_to :assignment
   belongs_to :student
+  
 end
