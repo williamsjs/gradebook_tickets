665c3fdf44415ab60b6bf8ce87a8d7376c5f8eba
John M Buscemi <jmbuscemi@gmail.com>
2015-06-25 14:34:03 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Teacher.create(name: "Test", email: "test@test.com", password: "password")
-Student.create(name: "Student", email: "student@student.com", password: "password", teacher_id: 1)
-Parent.create(name: "Parent", email: "parent@parent.com", password: "password", student_id: 1)
+Teacher.create!(name: "Test", email: "test@test.com", password: "password")
+Student.create!(name: "Student", email: "student@student.com", password: "password", teacher_id: 1)
+Parent.create!(name: "Parent", email: "parent@parent.com", password: "password", student_id: 1)
+
+
+bobby = Student.create!(name: "Bobby Tables", email: "bobby@student.com", password: "password", teacher_id: 1)
+susy = Student.create!(name: "Susy Q", email: "susy@student.com", password: "password", teacher_id: 1)
+jimmy = Student.create!(name: "Jimmy", email: "jimmy@student.com", password: "password", teacher_id: 1)
+
+assignment = Assignment.create!(name: "Algebra Lesson 1", due: Time.now)
+
+Grade.create!(score: 35, assignment_id: assignment.id, student_id: bobby.id)
+Grade.create!(score: 85, assignment_id: assignment.id, student_id: susy.id)
+Grade.create!(score: 93, assignment_id: assignment.id, student_id: jimmy.id)
