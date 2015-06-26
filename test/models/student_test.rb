8bee6f2e579aa5704eafb4174c0173687ce6900b
JohnDGreiner <john.d.greiner@gmail.com>
2015-06-26 09:20:59 -0400

    Test get_grade method.

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "get grade" do
+     test_student = students(:three)
+     assignment_test = assignments(:one)
+     assert_equal 100, test_student.get_grade(assignment_test.id)
+   end
 end
